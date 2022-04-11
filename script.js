c7613fc38cf13f79fc415b7e1eb6f397d0c4d32e
Alex <alexandrulapshin@gmail.com>
2022-04-11 17:05:46 -0400

    Create UI btns for player choice and add listeners

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
-//create game logic that takes 5 rounds and display the winner
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
+//game logic that takes 5 rounds and display the winner
+// function game(){
+//     let playerScore = 0;
+//     let computerScore = 0;
 
-    for(i=0; i<5; i++){
-        let playerSelect = window.prompt("Please enter your choice", "rock, scissors, paper");
-        let result = playRound(playerSelect, computerPlay());
-        if(result == "won") {alert("You won this round");playerScore++}
-        else if(result == "lost") {alert("You lost this round");computerScore++;}
-        else if(result == "tie"){alert("it's a tie")}
-        else {alert("Wrong value"); return};
-    }
+//     for(i=0; i<5; i++){
+//         let playerSelect = window.prompt("Please enter your choice", "rock, scissors, paper");
+//         let result = playRound(playerSelect, computerPlay());
+//         if(result == "won") {alert("You won this round");playerScore++}
+//         else if(result == "lost") {alert("You lost this round");computerScore++;}
+//         else if(result == "tie"){alert("it's a tie")}
+//         else {alert("Wrong value"); return};
+//     }
 
-    let winner = (playerScore > computerScore) ? "Congrats, you won the game!!!":
-        (playerScore < computerScore) ? "You lost the game. Sorry.":
-        "TIE! Try again? y/n";
+//     let winner = (playerScore > computerScore) ? "Congrats, you won the game!!!":
+//         (playerScore < computerScore) ? "You lost the game. Sorry.":
+//         "TIE! Try again? y/n";
         
-    return winner;
-}
+//     return winner;
+// }
 
-//create opponent/computer logic
+
+const buttons = document.querySelectorAll('button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const playerChoice = (button.id == 'rock')? "rock": 
+            (button.id == 'paper')? "paper" : "scissors";
+        playRound(playerChoice, computerPlay())}
+    )
+});
+// ///////////////////////////////////////////////////////////////////////////
+// SAME AS >>>>
+//
+// const rockBtn = document.querySelector('#rock')
+// rockBtn.addEventListener('click', () => playRound('rock', computerPlay()));
+
+// const paperBtn = document.querySelector('#paper')
+// paperBtn.addEventListener('click', () => playRound('paper', computerPlay()));
+
+// const scissorsBtn = document.querySelector('#scissors')
+// scissorsBtn.addEventListener('click', () => playRound('scissors', computerPlay()));
+// ///////////////////////////////////////////////////////////////////////////
+
+
+
+//opponent/computer
 function computerPlay(){
     let randNum = Math.floor(Math.random()*3);
     return (randNum == 0) ? "rock": (randNum == 1) ? "paper": "scissors"; 
 }
 
 
-//create game round logic
+//game round
 function playRound(playerChoice, computerChoice){
+    playerChoice
     playerChoice = playerChoice.toLowerCase();
 
-    let result = hasWrongVal(playerChoice) ? "error":
-        (playerChoice == computerChoice) ? "tie": 
+    let result = (playerChoice == computerChoice) ? "tie": 
         ((playerChoice == "rock" && computerChoice == "paper") ||
         (playerChoice == "scissors" && computerChoice == "rock") ||
         (playerChoice == "paper" && computerChoice == "scissors")) ? "lost": "won";
+
+        console.log("Player:"+ playerChoice);
+        console.log("Computer:"+ computerChoice);
+        console.log(result);
     return result;
 }
-
-function hasWrongVal(value){
-    return (value == "rock" || value == "paper" || value == "scissors")? false: true;
-}
-
 
