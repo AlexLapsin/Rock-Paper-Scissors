da3a22d3904abe1d37fc7598c5973cfdc004ed70
Alex <alexandrulapshin@gmail.com>
2022-04-14 14:32:19 -0400

    Display the winner on page and clean up code

--- script.js
+++ script.js
@@ -1,51 +1,45 @@
 
-//     let winner = (playerScore > computerScore) ? "Congrats, you won the game!!!":
-//         (playerScore < computerScore) ? "You lost the game. Sorry.":
-//         "TIE! Try again? y/n";
-        
-//     return winner;
-// }
 
 
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 const buttons = document.querySelectorAll('button')
 const displayDiv = document.querySelector("#displayResults");
 const scoreText = document.createElement('h3');
+const winnerText = document.createElement('h1');
 
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', () => {
         const playerChoice = (button.id == 'rock')? "rock": 
             (button.id == 'paper')? "paper" : "scissors";
-        
+
         displayDiv.appendChild(scoreText);
-        displayScore(playRound(playerChoice, computerPlay()))
-        
-        }
-    )
+        if(round>=5){
+            displayWinner(); return}
+        else{
+            displayScore(playRound(playerChoice, computerPlay()))
+            round++;
+            };       
+    });
 });
-// ///////////////////////////////////////////////////////////////////////////
-// SAME AS >>>>
-//
-// const rockBtn = document.querySelector('#rock')
-// rockBtn.addEventListener('click', () => playRound('rock', computerPlay()));
-
-// const paperBtn = document.querySelector('#paper')
-// paperBtn.addEventListener('click', () => playRound('paper', computerPlay()));
-
-// const scissorsBtn = document.querySelector('#scissors')
-// scissorsBtn.addEventListener('click', () => playRound('scissors', computerPlay()));
-// ///////////////////////////////////////////////////////////////////////////
 
 
 function displayScore(result){
-
-    
-
     (result == "won")? playerScore++ : (result == "lost")? computerScore++: 
         (playerScore++, computerScore++);
     scoreText.textContent = `Player: ${playerScore}  Computer: ${computerScore}`;
 }    
+
+
+function displayWinner(){
+    let winner = (playerScore > computerScore) ? "Congrats, you won the game!!!":
+        (playerScore < computerScore) ? "You lost the game. Sorry.":
+        "TIE! Try again? y/n";
+        
+    winnerText.textContent = winner;
+    displayDiv.appendChild(winnerText);
+}
 
 //opponent/computer
 function computerPlay(){
@@ -56,9 +50,7 @@ function computerPlay(){
 
 //game round
 function playRound(playerChoice, computerChoice){
-    playerChoice
     playerChoice = playerChoice.toLowerCase();
-
     let result = (playerChoice == computerChoice) ? "tie": 
         ((playerChoice == "rock" && computerChoice == "paper") ||
         (playerChoice == "scissors" && computerChoice == "rock") ||
